e071b311fd5bc5f1795e6a5650eb97f33b455420
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 15:02:31 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "3cb57f65-7f29-415f-a9b6-6229d080e811",
+    "home_feed": [
+        {
+            "id": "c4168bc0-18f5-4df2-bee6-2567621b60ad",
+            "artist_name": "sister",
+            "home_carousel": [
+                {
+                    "id": "38ad78f6-25a2-40cc-b0e5-16978e1aa2f4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png"
+                },
+                {
+                    "id": "112c3878-6602-4274-b340-d4b688aaf440",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png"
+                },
+                {
+                    "id": "b4d5c273-4fc4-43ca-94b9-7008c4e4c5d5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/36.png"
+                },
+                {
+                    "id": "a07ad43e-ca4c-4f0b-aacd-dfdbeec531c6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/173.png"
+                },
+                {
+                    "id": "1a27179f-5d5b-4235-a2de-d7af036bdd9e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/304.png"
+                }
+            ],
+            "art_caption": "federal",
+            "art_description": "Whose politics ever yourself daughter learn visit action. Environmental sit house significant."
+        },
+        {
+            "id": "cb898b8a-c058-4961-86c8-a3a6d9c89e43",
+            "artist_name": "boy",
+            "home_carousel": [
+                {
+                    "id": "1da0a393-ec74-40e5-aea3-87d075698461",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+                },
+                {
+                    "id": "77f04973-3e42-4489-ad3d-4b873855a3a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "d821e8ae-53b7-47d5-88d9-c36ab46ce6fa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/339.png"
+                },
+                {
+                    "id": "13db8951-0928-435c-8d1e-faa05a4936b7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/229.png"
+                },
+                {
+                    "id": "3afe917b-f8ee-4192-897d-442b9fdbf16e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png"
+                }
+            ],
+            "art_caption": "can",
+            "art_description": "Until authority explain use. East continue quickly near material generation. Issue event ability truth space approach."
+        },
+        {
+            "id": "7cb26bdf-c323-4a14-92fd-f13094f6d4e0",
+            "artist_name": "fine",
+            "home_carousel": [
+                {
+                    "id": "43d92f5a-e257-448d-84e3-b22945e10ca3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/100.png"
+                },
+                {
+                    "id": "f0e6cbd1-889d-4536-ad37-542c3f172990",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/245.png"
+                },
+                {
+                    "id": "ddb39bb0-57ca-47bb-a927-68cf3fac47fc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/57.png"
+                },
+                {
+                    "id": "3dabfbea-11f8-4476-ace8-151050bd740d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/36.png"
+                },
+                {
+                    "id": "522d0d05-0ecb-46b6-b407-f499af2698d0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/46.png"
+                }
+            ],
+            "art_caption": "relationship",
+            "art_description": "Trouble example area star girl daughter matter education. Could try also as those. Seat culture reflect significant shake everything article."
+        },
+        {
+            "id": "083d89b9-6aa6-4279-9d4a-c526a5be5e6e",
+            "artist_name": "billion",
+            "home_carousel": [
+                {
+                    "id": "675d62e3-d032-4156-b37b-2bebea630b30",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/293.png"
+                },
+                {
+                    "id": "18f99a1f-f491-453d-84c8-11cb49e2cb5f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/235.png"
+                },
+                {
+                    "id": "033b1c19-9013-4e27-be47-500f3515c0dd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/362.png"
+                },
+                {
+                    "id": "147d221f-ebd2-4a17-a54f-69a264e0d286",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/360.png"
+                },
+                {
+                    "id": "8d7cd5d6-a200-4a0a-927e-bfd506013f45",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+                }
+            ],
+            "art_caption": "garden",
+            "art_description": "Give admit perhaps size way himself above. Remember player rock different student."
+        },
+        {
+            "id": "a7235762-e4a1-479f-a331-c947de2d6b5a",
+            "artist_name": "walk",
+            "home_carousel": [
+                {
+                    "id": "589d9616-66a1-459f-bd64-3b0f47e3a71c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/77.png"
+                },
+                {
+                    "id": "abbb5642-cf48-4d33-80ec-1853641640d9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png"
+                },
+                {
+                    "id": "01544eca-b748-452e-9e09-c229b863b481",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/63.png"
+                },
+                {
+                    "id": "db6e8415-4767-4c3e-9cd8-730702034cc7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/134.png"
+                },
+                {
+                    "id": "d53551f7-2468-4dbe-a56e-01ff6bacd54c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/367.png"
+                }
+            ],
+            "art_caption": "heart",
+            "art_description": "Set agency health class administration ok morning. Sit dinner husband want town pressure. Kind science we issue."
+        }
+    ]
+}
